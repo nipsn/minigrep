db4f00a2e6656c4170b2f40f3de980fc0465dc50
nipsn <oscar.nydza@pm.me>
2023-09-30 17:02:14 +0200

    initial refactor with struct

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,22 @@ fn main() {
         Cool note: you can use args.iter() to unlock map and flatMap!
      */
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let content = fs::read_to_string(file_path)
+    let content = fs::read_to_string(config.file_path)
         .expect("What you doin brother. No file there");
 
     println!("File text:\n{content}")
+}
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
+fn parse_config(args: &[String]) -> Config {
+    Config { query: args[1].clone(), file_path: args[2].clone() }
 }
