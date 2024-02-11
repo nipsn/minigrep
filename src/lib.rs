8bb80af390d6b7c0689ebebd26cd9ea2168333a1
nipsn <oscar.nydza@pm.me>
2024-02-11 19:13:07 +0100

    Added functional search

--- src/lib.rs
+++ src/lib.rs
@@ -17,8 +17,32 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let content = fs::read_to_string(config.file_path)?;
-
-    println!("File text:\n{content}");
+    
+    for line in search(&config.query, &content) {
+        println!("{line}");
+    }
 
     Ok(())
 }
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    // functinoal search !!
+    content.lines().filter(|line| {line.contains(query)}).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
